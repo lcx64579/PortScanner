67a184c7f30e046fdf2fd471e9bd07854687b27c
GreatSeal <lcx64579@163.com>
2020-06-25 22:15:45 +0800

    first commit

---
+++ moc_mainwindow.cpp
@@ -0,0 +1,172 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[16];
+    char stringdata0[186];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 9), // "startScan"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 4), // "host"
+QT_MOC_LITERAL(4, 27, 9), // "startPort"
+QT_MOC_LITERAL(5, 37, 7), // "endPort"
+QT_MOC_LITERAL(6, 45, 7), // "timeout"
+QT_MOC_LITERAL(7, 53, 24), // "on_pBtnStartScan_clicked"
+QT_MOC_LITERAL(8, 78, 20), // "on_pBtnPause_clicked"
+QT_MOC_LITERAL(9, 99, 19), // "on_pBtnExit_clicked"
+QT_MOC_LITERAL(10, 119, 12), // "updateStatus"
+QT_MOC_LITERAL(11, 132, 4), // "text"
+QT_MOC_LITERAL(12, 137, 14), // "updateProgress"
+QT_MOC_LITERAL(13, 152, 5), // "value"
+QT_MOC_LITERAL(14, 158, 11), // "cleanStatus"
+QT_MOC_LITERAL(15, 170, 15) // "scannerFinished"
+
+    },
+    "MainWindow\0startScan\0\0host\0startPort\0"
+    "endPort\0timeout\0on_pBtnStartScan_clicked\0"
+    "on_pBtnPause_clicked\0on_pBtnExit_clicked\0"
+    "updateStatus\0text\0updateProgress\0value\0"
+    "cleanStatus\0scannerFinished"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       8,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    4,   54,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       7,    0,   63,    2, 0x08 /* Private */,
+       8,    0,   64,    2, 0x08 /* Private */,
+       9,    0,   65,    2, 0x08 /* Private */,
+      10,    1,   66,    2, 0x0a /* Public */,
+      12,    1,   69,    2, 0x0a /* Public */,
+      14,    0,   72,    2, 0x0a /* Public */,
+      15,    0,   73,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString, QMetaType::Int, QMetaType::Int, QMetaType::Int,    3,    4,    5,    6,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   11,
+    QMetaType::Void, QMetaType::Int,   13,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->startScan((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])),(*reinterpret_cast< int(*)>(_a[4]))); break;
+        case 1: _t->on_pBtnStartScan_clicked(); break;
+        case 2: _t->on_pBtnPause_clicked(); break;
+        case 3: _t->on_pBtnExit_clicked(); break;
+        case 4: _t->updateStatus((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 5: _t->updateProgress((*reinterpret_cast< const int(*)>(_a[1]))); break;
+        case 6: _t->cleanStatus(); break;
+        case 7: _t->scannerFinished(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (MainWindow::*_t)(QString , int , int , int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::startScan)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 8)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 8;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::startScan(QString _t1, int _t2, int _t3, int _t4)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)), const_cast<void*>(reinterpret_cast<const void*>(&_t3)), const_cast<void*>(reinterpret_cast<const void*>(&_t4)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
